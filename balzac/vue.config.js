8dc77239937cf80dbf9d9c433e901c51be9c99c2
wodedow <wodehuangne@gmail.com>
2020-07-16 12:57:48 +0800

    xugh

---
+++ balzac/vue.config.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//   devServer: {
+//     host: '10.206.0.15',
+//     port: 8080,
+//     headers: {
+//       'Access-Control-Allow-Origin': '*',
+//     },
+//     hotOnly: false,
+//     inline: false,
+//     disableHostCheck: true
+//   }
+// }
